7ed9cbac21dd51d616032b2304c79c0850319851
aihamjarrah <ayham.mjarr@gmail.com>
2020-10-29 23:20:47 +0300

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Cars from "./components/Cars";
 import About from "./components/about";
 import AddCar from "./components/addCar"
-
+import axios from 'axios';
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
+  componentDidMount(){
+    this.getAllCars()
+
+  }
+  getAllCars=()=>{
+    axios
+       .get("http://localhost:3001/cars")
+       .then((response)=>{
+         console.log("Response",response)
+       })
+       .catch((err)=>{
+         console.log("Result",err)
+       })
+  }
+  getAllUsers=()=>{
+    axios
+         .get("http://localhost:3001/users")
+         .then((response)=>{
+           if(response.status!==200){
+             console.log("You need to login in first")
+             this.setState({isLoggedIn:false})
+
+             
+           }
+           else{
+             console.log("Access granted")
+             this.setState({isLoggedIn:true})
+           }
+         })
+         .catch((err)=>{
+           console.log("Result",err)
+         })
+  }
+
   render() {
     return (
       <Router>
@@ -29,7 +69,7 @@ export default class App extends Component {
             <Home />
           </Route>
           <Route exact path="/cars">
-            <Cars />
+            <Cars getCars={this.getAllCars}/>
           </Route>
           <Route exact path="/about">
             <About />
--- src/components/Cars.js
+++ src/components/Cars.js
@@ -4,7 +4,9 @@ export default class Cars extends Component {
   render() {
     return (
       <div className="cars">
-        <h1>Cars</h1>
+        <h1>Here you can find the car you need</h1>
+        
+        <button onClick={this.props.getCars}>All Cars</button>
       </div>
     );
   }
